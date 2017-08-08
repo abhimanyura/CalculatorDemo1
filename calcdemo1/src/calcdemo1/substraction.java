71d7d3a7e619bf988a6f0d8ca9c863044195936a
abhimanyura <abhimanyu.ise@gmail.com>
2017-08-08 16:45:07 +0530

    from second branch

--- calcdemo1/src/calcdemo1/substraction.java
+++ calcdemo1/src/calcdemo1/substraction.java
@@ -2,5 +2,12 @@ package calcdemo1;
 
 public class substraction {
 
-	
+	public void substract(double a,double b)
+	{
+		 a=10.5;
+		b=20.5;
+		double c;
+		c=b-a;
+		System.out.println("substraction Resut is"+c);
+	}
 }
